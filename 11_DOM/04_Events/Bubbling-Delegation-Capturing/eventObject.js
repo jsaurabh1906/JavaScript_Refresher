987c773d7e8d5775e8cf4d6ff1de6204ba5213fd
Saurabh Jaiswal <jsaurabh1906@gmail.com>
2026-04-25 19:09:51 +0530

    feat: DOM Events: preventDefault and stopPropagation

--- 11_DOM/04_Events/Bubbling-Delegation-Capturing/eventObject.js
+++ 11_DOM/04_Events/Bubbling-Delegation-Capturing/eventObject.js
@@ -23,3 +23,26 @@ button.addEventListener(
 // - currentTarget: The element to which the event handler is attached.
 // - preventDefault(): A method to prevent the default action of the event.
 // - stopPropagation(): A method to stop the event from bubbling up the DOM tree.
+
+// preventDefault() Example:
+document.getElementById("link").addEventListener(
+  "click",
+  function (event) {
+    event.preventDefault(); // This will prevent the default action of the link (e.g., navigating to the URL)
+    console.log("Link clicked (but default action prevented)");
+  },
+  false,
+);
+
+// stopPropagation() is used to stop the event from bubbling up the DOM tree,
+// which means that the event will not trigger any event handlers on ancestor elements.
+// This can be useful when we want to handle an event on a specific element and prevent it from affecting other elements.
+// stopPropagation() Example:
+document.getElementById("child-3").addEventListener(
+  "click",
+  function (event) {
+    console.log("child li-3 cliked");
+    event.stopPropagation(); // This will stop the event from bubbling up
+  },
+  false,
+);
